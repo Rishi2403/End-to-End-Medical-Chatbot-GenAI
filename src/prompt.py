83b8b23a0b3bcb682c52e53dee9ee6502bbfd8ad
Rishi Bhattasali <rishibhattasali@gmail.com>
2025-06-28 13:20:54 +0530

    Created Complete chain and prepared modular coding

---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+
+system_prompt =(
+    "You are an assistant for a question answering system. "
+    "Use the following pieces of context to answer the question. "
+    "the question . If you don't know the answer, just say that you don't know, "
+    "Use five sentences maximum to answer the question. "
+    "\n\n"
+    "{context}"
+)
